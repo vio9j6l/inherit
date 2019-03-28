7506c8ba8e5bb393cabf59f019a9d431d5267944
vio9j6l <jiajia.liu951ss@gmail.com>
2019-03-28 10:25:30 -0400

    update

--- src/com/company/Point.java
+++ src/com/company/Point.java
@@ -5,12 +5,6 @@ public class Point {
     private double y;
     private double z;
 
-    public Point(double _x, double _y, double _z) {
-        this.x= _x;
-        this.y = _y;
-        this.z = _z;
-    }
-
     public Point(double _x, double _y) {
         this.x= _x;
         this.y = _y;
@@ -22,6 +16,4 @@ public class Point {
     public double getY() {return y;}
     public void setY(double _y) {this.y = _y;}
 
-    public double getZ() {return z;}
-    public void setZ(double _z) {this.z = _z;}
 }
--- src/com/company/Triangle.java
+++ src/com/company/Triangle.java
@@ -2,11 +2,11 @@ package com.company;
 
 public class Triangle extends Shape {
     public Triangle() {
-        p1 = new Point(2,8,9);
-        System.out.println("Triangle Point: X:" + p1.getX() + " Y:" + p1.getY() + " Z:" + p1.getZ());
+        p1 = new Point(2,8);
+        System.out.println("Triangle Point: X:" + p1.getX() + " Y:" + p1.getY());
     }
-    public Triangle(Point p1) {
-        System.out.println("Triangle Point: X:" + p1.getX() + " Y:" + p1.getY() + " Z:" + p1.getZ());
 
+    public Triangle(Point p1) {
+        System.out.println("Triangle Point: X:" + p1.getX() + " Y:" + p1.getY() + " Z:");
     }
 }
